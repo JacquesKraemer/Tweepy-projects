471a461a7e6c55e2c56b761552bc76146d560b2d
jacques kraemer <jacqueskraemer2001@gmail.com>
2024-10-14 16:48:43 -0300

    Primer commit

---
+++ dolar_scraper.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import tweepy
+
+url = "https://dolarhoy.com/"
+dolar_hoy = requests.get(url)
+
+if dolar_hoy.status_code == 200:
+    soup = BeautifulSoup(dolar_hoy.content, 'html.parser')
+
+    # Buscar todas las cotizaciones
+    cotizaciones = soup.find_all("div", class_="tile is-child")
+
+    actualizado = soup.find("div", class_="tile is-child")
+    hora_cotización = actualizado.find("div", class_="tile update")
+    hora_cotización = hora_cotización.text.strip().lower()
+
+    # Lista para almacenar los datos
+    datos_cotizaciones = []
+
+    for cotizacion in cotizaciones:
+        title = cotizacion.find("a", class_="title")
+        values = cotizacion.find("div", class_="values")
+
+        if values:
+            venta = values.find("div", class_="venta")
+            variacion = venta.find(
+                "div", class_="var-porcentaje") if venta else None
+
+            # Extraer valores de venta
+            valor_venta = venta.find(
+                "div", class_="val").text.strip() if venta else "No disponible"
+
+            # Extraer variación como valor numerico
+            try:
+                valor_variacion = variacion.text.strip().replace('%', '')
+                valor_variacion = float(valor_variacion)
+            except Exception:
+                valor_variacion = variacion.text.strip().replace('%', '').replace(',', '.')
+                valor_variacion = float(valor_variacion)
+
+            # Guardar datos en la lista si variación es distinto a cero
+            if valor_variacion != 0:
+                datos_cotizaciones.append({
+                    "Nombre": title.text.strip() if title else "No disponible",
+                    "Venta": valor_venta,
+                    "Variación": valor_variacion
+                })
+            else:
+                pass
+else:
+    print("Error al obtener el HTML")
+
+df_cotizaciones = pd.DataFrame(datos_cotizaciones)
+lista_textos = []
+
+for index, row in df_cotizaciones.iterrows():
+    nombre = row['Nombre']
+    venta = row['Venta']
+    variacion = row['Variación']
+
+    texto = f"El {nombre}, cotiza a {venta} con una variación del {variacion}%."
+
+    lista_textos.append(texto)
+
+bloque_texto = "\n".join(lista_textos)
+
+# Agregar la hora de cotización al inicio del bloque de texto
+texto_cotizaciones = f"Informe cambiario, {hora_cotización}:\n{bloque_texto}"
+
+print(texto_cotizaciones)
+
+# Crear un tweet
+consumer_key =
+consumer_secret =
+access_token =
+access_token_secret =
+
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+response = client.create_tweet(text=texto_cotizaciones)
