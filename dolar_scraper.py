2a48a694af9a2bd400a727cf48d38f1aaa6e3b50
jacques kraemer <165099319+JacquesKraemer@users.noreply.github.com>
2024-10-15 13:26:05 -0300

    Update dolar_scraper.py

--- dolar_scraper.py
+++ dolar_scraper.py
@@ -74,12 +74,14 @@ print(texto_cotizaciones)
 
 # Crear un tweet
 try:
+    bearer_token = os.environ["bearer_token"]
     consumer_key = os.environ["consumer_key"]
     consumer_secret = os.environ["consumer_secret"]
     access_token = os.environ["access_token"]
     access_token_secret = os.environ["access_token_secret"]
 
     client = tweepy.Client(
+        bearer_token=bearer_token,
         consumer_key=consumer_key, consumer_secret=consumer_secret,
         access_token=access_token, access_token_secret=access_token_secret
     )
